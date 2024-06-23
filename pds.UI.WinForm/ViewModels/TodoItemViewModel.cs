3d857046cecb27c18476314a90f728bcbfd6b167
pixcelo <bodomu@gmail.com>
2024-06-23 11:29:05 +0900

    add: ViewModel

---
+++ pds.UI.WinForm/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.CodeDom;
+
+namespace pds.UI.WinForm.ViewModels;
+
+public class TodoItemViewModel : ViewModelBase
+{
+    public TodoItemViewModel()
+    {
+        
+    }
+}
+
---
+++ pds.UI.WinForm/ViewModels/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace pds.UI.WinForm.ViewModels;
+
+public abstract class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// プロパティの変更を通知する
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="field"></param>
+    /// <param name="value"></param>
+    /// <param name="propertyName"></param>
+    /// <returns></returns>
+    protected bool SetProperty<T>(
+        ref T field,
+        T value,
+        [CallerMemberName] string? propertyName = null)
+    {
+        if (Equals(field, value)) return false;
+
+        field = value;
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        return true;
+    }
+}
