080d52df3f012badaf40c9f1f918e84fd853ff38
pixcelo <bodomu@gmail.com>
2024-06-29 03:32:48 +0900

    add: infrastructure

---
+++ pds.Infrastructure/SQLite/SQLiteHelper.cs
@@ -0,0 +1,166 @@
+﻿using System.Data.SQLite;
+
+namespace pds.Infrastructure.SQLite;
+
+internal class SQLiteHelper
+{
+    internal const string ConnectionString = @"Data Source=pds.db;Version=3;";
+
+    /// <summary>
+    /// 全件取得
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="sql"></param>
+    /// <param name="CreateEntity"></param>
+    /// <returns></returns>
+    internal static IReadOnlyList<T> Query<T>(
+        string sql,
+        Func<SQLiteDataReader, T> CreateEntity)
+    {
+        return Query(sql, null, CreateEntity);
+    }
+
+    /// <summary>
+    /// 全件取得
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="sql"></param>
+    /// <param name="parameters"></param>
+    /// <param name="CreateEntity"></param>
+    /// <returns></returns>
+    internal static IReadOnlyList<T> Query<T>(
+        string sql,
+        SQLiteParameter[]? parameters, 
+        Func<SQLiteDataReader, T> CreateEntity)
+    {
+        var result = new List<T>();
+
+        using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
+        using (var command = new SQLiteCommand(sql, connection))
+        {
+            connection.Open();
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }                
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    result.Add(CreateEntity(reader));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// 1件のみ取得する nullEntiryはnullを返却するためのもの
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="sql"></param>
+    /// <param name="CreateEntity"></param>
+    /// <param name="nullEntity"></param>
+    /// <returns></returns>
+    internal static T QuerySingle<T>(
+        string sql,            
+        Func<SQLiteDataReader, T> CreateEntity,
+        T nullEntity)
+    {
+        return QuerySingle(sql, null, CreateEntity, nullEntity);
+    }
+
+    /// <summary>
+    /// 1件のみ取得する nullEntiryはnullを返却するためのもの
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="sql"></param>
+    /// <param name="parameters"></param>
+    /// <param name="CreateEntity"></param>        
+    /// <param name="nullEntity"></param>
+    /// <returns></returns>
+    internal static T QuerySingle<T>(
+        string sql,
+        SQLiteParameter[]? parameters,
+        Func<SQLiteDataReader, T> CreateEntity,            
+        T nullEntity)
+    {
+        using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
+        using (var command = new SQLiteCommand(sql, connection))
+        {
+            connection.Open();
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    return CreateEntity(reader);
+                }
+            }
+        }
+
+        return nullEntity;
+    }
+
+    /// <summary>
+    /// 更新または追加を実行する（アップサート）
+    /// </summary>
+    /// <param name="insert"></param>
+    /// <param name="update"></param>
+    /// <param name="parameters"></param>
+    internal static void Execute(
+        string insert,
+        string update,
+        SQLiteParameter[] parameters)
+    {
+        using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
+        using (var command = new SQLiteCommand(update, connection))
+        {
+            connection.Open();
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            // 件数が返却されない（実行できない）場合、インサート実行
+            if (command.ExecuteNonQuery() < 1)
+            {
+                command.CommandText = insert;
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    /// <summary>
+    /// クエリを実行する
+    /// </summary>
+    /// <param name="sql"></param>
+    /// <param name="parameters"></param>
+    internal static void Execute(
+        string sql,
+        SQLiteParameter[] parameters)
+    {
+        using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
+        using (var command = new SQLiteCommand(sql, connection))
+        {
+            connection.Open();
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            command.ExecuteNonQuery();
+        }
+    }
+}
+
---
+++ pds.Infrastructure/SQLite/TodoItemSQLite.cs
@@ -0,0 +1,28 @@
+﻿using pds.Domain.Entities;
+using pds.Domain.Repositoriers;
+
+namespace pds.Infrastructure.SQLite
+{
+    public class TodoItemSQLite : ITodoItemRepository
+    {
+        public IReadOnlyList<TodoItemEntity> GetTodoItems()
+        {
+            var sql = @"SECELT * FROM TodoItems";
+
+            return SQLiteHelper.Query(sql,
+                reader =>
+                {
+                    return new TodoItemEntity(
+                        Convert.ToInt32(reader["TodoItemId"]),
+                        Convert.ToString(reader["Title"]),
+                        Convert.ToString(reader["Description"]),
+                        Convert.ToBoolean(reader["IsCompleted"]));
+                });
+        }
+
+        public void Save(TodoItemEntity todoItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
